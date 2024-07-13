bd2e810119a81d998083b649ac3ecac2b4f324be
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2024-07-13 14:47:00 +0330

    html & css code is completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,36 +3,38 @@ module.exports = {
   content: ["./pages/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        spaceGrotesk:["Space Grotesk"]
+      fontFamily: {
+        spaceGrotesk: ["Space Grotesk"],
       },
       colors: {
-        green:'#B9FF66',
-        gray:'#F3F3F3',
-        dark:'#191A23',
-        black:'#000000'
+        green: "#B9FF66",
+        gray: "#F3F3F3",
+        dark: "#191A23",
+        footer: "#292A32",
       },
-      borderRadius:{
-        btn:'14px',
-        ttl:'7px'
+      borderRadius: {
+        btn: "14px",
+        ttl: "7px",
       },
-      margin:{
-        mside: '6.95%'
+      margin: {
+        mside: "6.95%",
       },
-      fontSize:{
-        content: '1.39vw',    //20px
-        p:'1.25vw',           //18px
-        h1:'4.17vw',          //60px
-        h2:'2.78vw',          //40px
-        h3:'2.08vw',          //30px
-        h4:'1.39vw',          //20px
+      fontSize: {
+        content: "1.39vw", //20px
+        p: "1.25vw", //18px
+        h1: "4.17vw", //60px
+        h2: "2.78vw", //40px
+        h3: "2.08vw", //30px
+        h4: "1.39vw", //20px
       },
-      padding:{
-        btn:'1.39vw 2.43vw',
-        ttl: '0 .49vw'
-      }
+      padding: {
+        btn: "1.39vw 2.43vw",
+        ttl: "0 .49vw",
+      },
+      accentColor: {
+        green: "#B9FF66",
+      },
     },
   },
   plugins: [],
-}
-
+};
